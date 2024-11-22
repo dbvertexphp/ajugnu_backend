1bfa78a3a4031687a3720f9e702d6fceaac36c68
dbvertexphp <php4.dbvertex@gmail.com>
2024-11-22 19:17:10 +0530

    code live

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -22,6 +22,12 @@ const Createcategory = asyncHandler(async (req, res) => {
       throw new ErrorHandler("Please enter all the required fields.", 400);
     }
 
+    // Check if the category already exists by name
+    const existingCategory = await Category.find({ category_name: category_name }); // Case insensitive check
+    if (existingCategory) {
+      return res.status(400).json({ message: "Category with this name already exists.", status: false });
+    }
+
     // Get the file path and normalize it to use forward slashes
     const categoryImage = req.file ? req.file.path.replace(/\\/g, "/") : null;
 
